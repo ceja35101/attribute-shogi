5aa3610b1fdf3b05afafc10befcbb17dd55cf7b6
ceja35101 <58505429+ceja35101@users.noreply.github.com>
2026-07-24 14:46:51 +0900

    Bust mobile style cache

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME="attribute-shogi-v39";
+const CACHE_NAME="attribute-shogi-v40";
 const ASSETS=[
   "./",
   "./index.html",
-  "./styles.css?v=58",
+  "./styles.css?v=59",
   "./game-core.js?v=13",
   "./app.js?v=38",
   "./attributes.json",
