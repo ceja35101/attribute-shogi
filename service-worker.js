edd9836f31520887457b0d79a52b217b58fb8a30
ceja35101 <58505429+ceja35101@users.noreply.github.com>
2026-07-24 12:57:51 +0900

    Enlarge elemental backgrounds behind pieces

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME="attribute-shogi-v30";
+const CACHE_NAME="attribute-shogi-v31";
 const ASSETS=[
   "./",
   "./index.html",
-  "./styles.css?v=49",
+  "./styles.css?v=50",
   "./game-core.js?v=13",
   "./app.js?v=36",
   "./attributes.json",
